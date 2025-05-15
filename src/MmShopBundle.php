7fb270ea39d2a962a45839738ba893b50680582b
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-05-15 19:32:44 +0200

    Update MmShopBundle.php

--- src/MmShopBundle.php
+++ src/MmShopBundle.php
@@ -3,20 +3,18 @@ namespace Bits\MmShopBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-//use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+
+
+
 
 class MmShopBundle extends Bundle
 {
     
      public function getPath(): string
     {
-        return \dirname(__DIR__);
+        return \dirname(__DIR__.'/Resources');
     }
+    
      public function build(ContainerBuilder $container): void
     {
      
@@ -24,11 +22,6 @@ class MmShopBundle extends Bundle
         $container->setAlias('mm_shop_bundle', self::class);
          
          parent::build($container);
-    }
-    protected function addCompilerPass($compilerPass, $type = PassConfig::TYPE_AFTER_REMOVING, $priority = 0)
-    {
-        // Dies stellt sicher, dass der CompilerPass korrekt registriert wird.
-        $this->container->addCompilerPass($compilerPass, $type, $priority);
     }
 
 }
