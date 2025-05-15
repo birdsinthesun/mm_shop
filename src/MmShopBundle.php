775802abd2665c45b2f6b82a7ad80038703e89c7
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-05-15 19:29:35 +0200

    dev

--- src/MmShopBundle.php
+++ src/MmShopBundle.php
@@ -13,21 +13,12 @@ use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 class MmShopBundle extends Bundle
 {
     
+     public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
      public function build(ContainerBuilder $container): void
     {
-        
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config')); 
-        $loader->load('services.yaml');
-        $loader->load('packages/twig.yaml');
-        
-        
-        $loader2 = new PhpFileLoader($container, new FileLocator(__DIR__.'/../config'));
-       
-        $loader2->load('bundles.php');
-        
-
-       // $container->addCompilerPass(new RemoveContaoCallbackPass(),PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
-       // $container->addCompilerPass(new MakeListenerPublicPass());
      
         // Füge einen Alias hinzu
         $container->setAlias('mm_shop_bundle', self::class);
