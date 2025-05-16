ad484ba1cb00c695b3fa6e5f3a0d323104a76c61
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2025-05-16 02:56:27 +0200

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,7 +1,8 @@
 <?php
 use Bits\MmShopBundle\Module\OrderingProcessModule;
+use Bits\MmShopBundle\Module\CartModule;
                 
 
 $GLOBALS['FE_MOD']['mm_shop']['ordering_process'] = OrderingProcessModule::class;
-$GLOBALS['FE_MOD']['mm_shop']['ordering_process'] = CartModule::class;
+$GLOBALS['FE_MOD']['mm_shop']['cart'] = CartModule::class;
   
